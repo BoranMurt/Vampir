6f81856b0be23f4df0fc5fee8fe7ff71006f0f58
PC <BoranMurt>
2022-01-05 23:43:59 +0300

    Working "deli" part

--- src/main/java/RolesNumber.java
+++ src/main/java/RolesNumber.java
@@ -2,36 +2,33 @@ import java.util.Scanner;
 
 public class RolesNumber {
 
-    public static int goodRoleNumber;
-    public static int badRoleNumber;
-    public static int neutralRoleNumber;
     //Set Good Roles Number
-    public Integer setGoodRolesNumber(){
-        Scanner myObj3= new Scanner(System.in);
+    public Integer setGoodRolesNumber() {
+        Scanner myObj3 = new Scanner(System.in);
         System.out.println("İyi rol sayısı: ");
-        String goodNumber=myObj3.nextLine();
+        String goodNumber = myObj3.nextLine();
         int goodRolesNumber = Integer.parseInt(goodNumber);
-        System.out.println("Belirlenen İYİ rol sayısı: "+goodRolesNumber);
+        System.out.println("Belirlenen İYİ rol sayısı: " + goodRolesNumber);
         return goodRolesNumber;
     }
 
     //Set Bad Roles Number
-    public Integer setBadRolesNumber(){
-        Scanner myObj4= new Scanner(System.in);
+    public Integer setBadRolesNumber() {
+        Scanner myObj4 = new Scanner(System.in);
         System.out.println("Kötü rol sayısı: ");
-        String badNumber=myObj4.nextLine();
+        String badNumber = myObj4.nextLine();
         int badRolesNumber = Integer.parseInt(badNumber);
-        System.out.println("Belirlenen KÖTÜ rol sayısı: "+badRolesNumber);
+        System.out.println("Belirlenen KÖTÜ rol sayısı: " + badRolesNumber);
         return badRolesNumber;
     }
 
     //Set Neutral Roles Number
-    public Integer setNeutralRolesNumber(){
-        Scanner myObj5= new Scanner(System.in);
+    public Integer setNeutralRolesNumber() {
+        Scanner myObj5 = new Scanner(System.in);
         System.out.println("Tarafsız rol sayısı: ");
-        String neutralNumber=myObj5.nextLine();
+        String neutralNumber = myObj5.nextLine();
         int neutralRolesNumber = Integer.parseInt(neutralNumber);
-        System.out.println("Belirlenen TARAFSIZ rol sayısı: "+neutralRolesNumber);
+        System.out.println("Belirlenen TARAFSIZ rol sayısı: " + neutralRolesNumber);
         return neutralRolesNumber;
     }
 }
